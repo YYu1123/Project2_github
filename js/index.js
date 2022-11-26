a5c6d17b4af149fdd7238f9b3a73bae970b0afd8
YYu1123 <2925708271@qq.com>
2022-11-26 21:31:30 +0800

    这是第七周的项目文件

--- js/index.js
+++ js/index.js
@@ -54,4 +54,106 @@ $(function (){
         $(this).find('.ebooks-title').hide(); //隐藏标题
         $(this).find('.desc').show(); //显示详情
     })
+    /*电子书tab切换*/
+    $('.ebooks-nav > li').mouseenter(function(){
+        $(this).addClass('active').siblings('li').removeClass('active');
+        var index = $(this).index();
+        $('.ebooks-list').eq(index).show().siblings('.ebooks-list').hide();
+    })
+
+    /* 服装tab切换 */
+     /* 服装小轮播 */     
+     $("#clothes-banner").tyslide({
+                boxh:352,//盒子的高度
+                w:472,//盒子的宽度        
+                h:352,//图片的高度        
+                isShow:true,//是否显示控制器        
+                isShowBtn:true,//是否显示左右按钮        
+                controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h        
+                controlsW:20,//控制按钮宽度        
+                controlsH:4,//控制按钮高度        
+                controlsColor:"#d7d7d7",//普通控制按钮的颜色        
+                controlsCurrentColor:"#f60000",//当前控制按钮的颜色    
+            }); 
+    /* 户外运动tab切换 */   
+    $("#sport-banner").tyslide({        
+        boxh:352,//盒子的高度        
+        w:472,//盒子的宽度        
+        h:352,//图片的高度        
+        isShow:true,//是否显示控制器        
+        isShowBtn:true,//是否显示左右按钮        
+        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h        
+        controlsW:20,//控制按钮宽度        
+        controlsH:4,//控制按钮高度        
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色        
+        controlsCurrentColor:"#f60000",//当前控制按钮的颜色    
+    });
+    /* 童装tab切换 */ 
+    $("#childeren-banner").tyslide({        
+        boxh:352,//盒子的高度        
+        w:472,//盒子的宽度        
+        h:352,//图片的高度        
+        isShow:true,//是否显示控制器        
+        isShowBtn:true,//是否显示左右按钮        
+        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h        
+        controlsW:20,//控制按钮宽度        
+        controlsH:4,//控制按钮高度        
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色        
+        controlsCurrentColor:"#f60000",//当前控制按钮的颜色    
+    });
+    
+    
+   
+    
+    /* 推广商品切换 */
+    $('.promotion .promotion-title ul li').mouseenter(function(){
+        // 导航激活类的切换
+        $(this).addClass('active').siblings().removeClass('active')
+        // 内容切换
+        // 获取对应的索引
+        var index=$(this).index(); //0 => left:0 * 1170, 1 => left:-1 * 1170,3 => left: -2 * 1170
+        //左右移动
+        $('.promotion .promotion-content .inner-box').animate({
+            'left':-index*1170
+        },300)
+    })
+
+    /* 二维码滑出效果 */ 
+    $('.qr-code .ticket').hover(function(){
+        // 让二维码滑出来
+        $('.qr-code div').stop(true).animate({
+            left:'-100px'
+        })
+    },function(){
+        // 让二维码收回去
+        $('.qr-code div').stop(true).animate({
+            left:'0'
+        })
+    })
+
+    /* 顶部搜索框交互 */ 
+    $(document).scroll(function(){
+        // 获取到顶部的距离
+        var topDistance=$('html,body').scrollTop();
+        // 判断
+        if(topDistance>500){
+            // 如果滚动距离大于500 滑下来
+            $('.top-search-box').slideDown(300)
+        }else{
+            // 否则 收回去
+            $('.top-search-box').slideUp(300)
+        }
+    })
+
+    /* 楼梯跳转 */ 
+    $('.floor li').click(function(){
+        // 获取索引
+        var index = $(this).index();
+        // 选中每一个板块到顶部偏移
+        var topOffset = $('.floorBox').eq(index).offset().top;
+        // 让滚动条滚到这个位置
+        $('html, body').animate({
+            scrollTop: topOffset - 50
+        })
+    })
 })
